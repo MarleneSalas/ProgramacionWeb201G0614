63da643262d77ecb5023179930be386f87c6730d
MarleneSalas <201g0614@rcarbonifera.tecnm.mx>
2023-09-28 22:36:13 -0600

    Unidad 1. Actividad 2

---
+++ U1A2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using U1A2.Models.ViewModels;
+
+namespace U1A2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(ConversionViewModel c)
+        {
+            if (c.Moneda == "USD")
+            {
+                c.Total = c.Importe / 18;
+                c.Tipo = "USD";
+            }
+            else
+            {
+                c.Total = c.Importe * 18;
+                c.Tipo = "MXN";
+            }
+            return View(c);
+        }
+    }
+}
---
+++ U1A2/Models/ViewModels/ConversionViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace U1A2.Models.ViewModels
+{
+    public class ConversionViewModel
+    {
+        public decimal Importe { get; set; }
+        public string? Moneda { get; set; }
+        public string? Tipo { get; set; }
+        public decimal Total { get; set; }
+    }
+}
