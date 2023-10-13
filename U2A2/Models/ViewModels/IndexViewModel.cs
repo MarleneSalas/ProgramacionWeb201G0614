bf0a835f7102cf3fae2c717f573c9a35a5acec6a
MarleneSalas <201g0614@rcarbonifera.tecnm.mx>
2023-10-13 00:43:50 -0600

    Creacion de proyecto final de Unidad 5Unidad 2. Actividad 2

---
+++ U2A2/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace U2A2.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<char> Letras { get; set; } = null!;
+        public IEnumerable<RazaModel> RazasPerro { get; set; }=null!;
+    }
+
+    public class RazaModel
+    {
+        public int IdRaza { get; set; }
+        public string Nombre{ get; set; } = null!;
+    }
+
+}
---
+++ U2A2/Models/ViewModels/RazaPaisViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Policy;
+
+namespace U2A2.Models.ViewModels
+{
+	public class RazaPaisViewModel
+	{
+		public string Nombre { get; set; } = null!;
+		public IEnumerable<RazaModel> Razas { get; set; } = null!;
+	}
+
+}
